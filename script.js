070dc25a5d85f873146c5e2a721b70d2e08eee17
Trong Doan <doantrong8777@gmail.com>
2022-06-21 15:22:43 -0700

    finished

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
-function increase(element){
-    var num = document.querySelector("#num");
-    num++;
-    
-}
+function increase(e){
+
+var likeId = e.nextElementSibling;
+var num = Number(likeId.firstChild.innerText) + 1;
+likeId.firstChild.innerText = num;
+console.log(num);
+ }
